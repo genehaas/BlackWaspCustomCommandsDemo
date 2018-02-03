b9fda2c80cb68ebe4f4bcd6aa5e4b79526d1c1db
Gene Haas <ghaas@hillercompanies.local>
2018-02-02 23:16:45 -0600

    Add project files.

---
+++ BlackWaspCustomCommandsDemo/AddCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BlackWaspCustomCommandsDemo
+{
+    class AddCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            return nameList != null
+                && !string.IsNullOrWhiteSpace(nameList.FirstName)
+                && !string.IsNullOrWhiteSpace(nameList.LastName);
+        }
+
+        public void Execute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            var newName = string.Format("{0} {1}", nameList.FirstName, nameList.LastName);
+            nameList.Names.Add(newName);
+            nameList.FirstName = nameList.LastName = "";
+        }
+    }
+}
---
+++ BlackWaspCustomCommandsDemo/RemoveCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BlackWaspCustomCommandsDemo
+{
+    class RemoveCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            return nameList != null && nameList.SelectedName != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            var nameList = parameter as NameList;
+            var oldName = nameList.SelectedName;
+            nameList.Names.Remove(oldName);
+        }
+    }
+}
